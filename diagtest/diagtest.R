91a3a07084b439bd37c64381963931c94dae24d0
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-05-29 15:17:39 +0700

    Use diagtest With (In) R Software

---
+++ diagtest/diagtest.R
@@ -0,0 +1,15 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Sensitivity and Specificity analysis Use diagtest With (In) R Software
+install.packages("lava")
+library("lava")
+diagtest = read.csv("https://raw.githubusercontent.com/timbulwidodostp/diagtest/main/diagtest/diagtest.csv",sep = ";")
+# Estimate Econometric Models for Spatial Panel Data Use spml With (In) R Software
+diagtest <- diagtest(diagtest,exact=TRUE)
+summary(diagtest)
+# Sensitivity and Specificity analysis Use diagtest With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
